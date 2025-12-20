1eda96f281f89febea6366284749d90aa865068f
teo <grecioiuteo@gmail.com>
2025-12-20 23:34:45 +0200

    Implementare structura de montare; functia de incarcare din fisier; comanda cdafiseaza acum MountPoint-urile gasite.

--- amsh.c
+++ amsh.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+typedef struct {
+	char sursa[70];
+	char destinatie[70];
+	int limita_timp;
+	long ultima_accesare;
+}Montare;
+
+Montare lista_montari[70];
+int nr_montari = 0;
+
+void configuratie(char *fisier_citire){
+	FILE *fisier = fopen(fisier_citire, "r");
+	if(fisier ==NULL) {
+	printf("Nu exista date in fisier");
+	}
+	nr_montari = 0;
+	while(fscanf(fisier, "%s %s %d",lista_montari[nr_montari].sursa, lista_montari[nr_montari].destinatie, &lista_montari[nr_montari].limita_timp) == 3)
+	{
+		lista_montari[nr_montari].ultima_accesare = 0;
+		nr_montari++;
+	}
+	fclose(fisier);
+	printf("S-au incarcat %d configuratii din fisier.\n", nr_montari);
+}
+
 int main(){
 	char line[1024];
 	char *comanda;
 	char *argument;
 	int lungime;
+
+	configuratie("test_configurare");
 	while(1) {
 		printf("amsh> ");
 		fflush(stdout);
@@ -29,8 +56,12 @@ int main(){
 		if (comanda != NULL) {
 
 			if (strcmp(comanda, "cd") == 0) {
-				printf("Ai cerut comanda interna cd\n");
-				}
+				printf("Lista de mountpoint-uri active:\n");
+				for (int i = 0; i<nr_montari; i++){
+					printf("%d. %s -> %s (%d secunde)\n", i+1, lista_montari[i].sursa, lista_montari[i].destinatie, lista_montari[i].limita_timp);
+				}				
+					
+			}
 			else {
 				printf("Ai cerut comanda externa: %s\n",comanda);
 				}
@@ -41,3 +72,4 @@ int main(){
 		}
 return 0;
 }
+
